822f21c1f8d1623206609b6c7156c8544fc74202
gcharang <21151592+gcharang@users.noreply.github.com>
2023-10-13 00:52:02 +0530

    add insight api endpoint for vrsc

--- src/lib/coins.js
+++ src/lib/coins.js
@@ -161,9 +161,8 @@ const coins = {
   VRSC: {
     explorer: 'https://insight.verus.io/',
     api: [
-      'https://insight.verus.io/api/',
+      'https://vrsc-explorer.lordofthechains.com/',
       'https://vrsc.explorer.dexstats.info/insight-api-komodo/',
-      'https://insight.vrsc.0x03.services/insight-api-komodo/',
     ],
     prices: {
       coinpaprika: 'vrsc-verus-coin',
